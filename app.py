1c6850b63cd3e28d79b7f8da5fb9124991b247f0
kijsacr <kennethij737@gmail.com>
2026-09-01 20:39:32 +0530

    updated the layout a bit

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("Hi Enoch")
+from flask import Flask, app, render_template, request,redirect, url_for
+import os
+
+app = Flask(__name__)
+
+
+# Home route
+@app.route('/', methods=['GET', 'POST'])
+def home():
+
+    #button action handling
+    if request.method == 'POST':
+        button_value = request.form.get('login-action')
+        if button_value == 'login':
+            return redirect(url_for("login"))
+    return render_template("home.html")
